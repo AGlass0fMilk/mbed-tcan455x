9f355a249f1420a447fd7bded427c3bf112bc207
George Beckstein <george.beckstein@gmail.com>
2020-12-11 18:46:20 -0500

    Update copyright notices

--- extensions/can_api.cpp
+++ extensions/can_api.cpp
@@ -1,12 +1,7 @@
-/**
- * ep-oc-mcu
- * Embedded Planet Open Core for Microcontrollers
- * 
- * Built with ARM Mbed-OS
- *
- * Copyright (c) 2019-2020 Embedded Planet, Inc.
+/*
+ * Copyright (c) 2020 George Beckstein
  * SPDX-License-Identifier: Apache-2.0
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,8 +12,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * limitations under the License
  */
 
 #include "hal/can_api.h"
